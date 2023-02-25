f652e4a941f0d0917bd3f3a678b4a5a0591d14c2
keremdokumaci <kerem.dokumaci@yemeksepeti.com>
2023-02-25 18:58:55 +0300

    add: sed instead of gsed

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -10,7 +10,7 @@ go test -v -coverpkg ./internal/... -coverprofile $(pwd)/coverage.out $(pwd)/...
 code_coverage_ratio=$(go tool cover -func $(pwd)/coverage.out | grep "total:" | awk '{print $3}')
 echo_colorized "Total test coverage: $code_coverage_ratio" "green"
 code_coverage_ratio_md=${code_coverage_ratio/%/25}
-gsed -i -r "s/coverage-[0-9\.\%]+/coverage-${code_coverage_ratio_md}/" $(pwd)/README.md
+sed -i '' -r "s/coverage-[0-9\.\%]+/coverage-${code_coverage_ratio_md}/" $(pwd)/README.md
 echo_colorized "README.md updated ..." "green"
 
 after_test
